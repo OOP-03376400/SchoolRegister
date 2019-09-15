04800999af7977c7f3f439a9516aa68c328e85da
Mateusz <wojtasiak.mateo@gmail.com>
2019-09-15 20:26:39 +0200

    Created default projects

---
+++ SchoolRegister.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SchoolRegister.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ SchoolRegister.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SchoolRegister.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
